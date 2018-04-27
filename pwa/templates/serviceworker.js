7f3d46bf7acbcd3ac52ad46cdfd3a1b6f2cd9efa
Devin Villarreal <devinjv@gmail.com>
2018-04-26 22:57:17 -0700

    Update base serviceworker template to solve "Site cannot be installed" issue

--- pwa/templates/serviceworker.js
+++ pwa/templates/serviceworker.js
@@ -1,2 +1,3 @@
-// Empty Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in
+self.addEventListener('fetch', function(event) {});
+// Base Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in
 // settings.py
